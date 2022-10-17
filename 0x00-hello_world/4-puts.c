e581412f879744cc1d1c639d8fb624b750f22f7e
Bukha042 <chukwuebukaarthur@gmail.com>
2022-10-17 08:13:21 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the phrase "programming is like building a multilingual puzzle
- * Return: 0 if existed properly, non-zero otherwise"
+ * Return: 0 if existed properly, non-zero otherwise
  */
 int main(void)
 {
