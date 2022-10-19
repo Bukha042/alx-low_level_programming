91baeb0d7ba181b29774f16fb1360210660fc5cb
Bukha042 <chukwuebukaarthur@gmail.com>
2022-10-18 20:12:51 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	_putchar("_putchar"\n");
+
+	return (0);
+
+}
