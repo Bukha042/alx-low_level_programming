b037033ed6012b93875ec4943dcab56ab2e15e3d
Bukha042 <chukwuebukaarthur@gmail.com>
2022-10-27 17:03:22 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character C to student
+ * @c: char to be printed
+ * Return: 1 on success
+ */
+int _putchar(char c)
+{
+	Return (write (1, &c, 1));
+}
